d9038c94a5a1e45201215a4e0611bee033583fdb
nikkrasn <krasnoyarov.nikita@gmail.com>
2015-05-24 12:28:25 +0400

    Added fp language&buildbot

--- server_project/server/build_bot_project/languages/fp_language.py
+++ server_project/server/build_bot_project/languages/fp_language.py
@@ -1,28 +1,37 @@
 import os
+import tempfile
+
+from common.cmd_utils import screen_str
 from build_bot_project.languages.ilanguage import ILanguage
 
 
 class FPLanguage(ILanguage):
 
-    COMPILER_FILE = "fp_compiler.exe"
+    COMPILER_FILE = "fpc.exe"
 
-    DEFAULT_COMPILER_DIR = "/path/to/fp/compiler"
+    DEFAULT_COMPILER_DIR = "C:\\FPC\\2.6.4\\bin\\i386-win32"
     DEFAULT_COMPILER_PATH = os.path.join(DEFAULT_COMPILER_DIR, COMPILER_FILE)
 
     def __init__(self, config_parser=None):
         ILanguage.__init__(self)
         self.compiler_dir = self.DEFAULT_COMPILER_DIR
-        self.compiler_path = os.path.join(self.compiler_dir, self.COMPILER_FILE)
+
         if config_parser:
             compiler_dir = config_parser.get('compiler_paths', 'fp_path')
             if compiler_dir:
                 self.compiler_dir = compiler_dir
+        self.compiler_path = os.path.join(self.compiler_dir, self.COMPILER_FILE)
 
     def get_compiler_path(self):
         return self.compiler_path
 
     def get_build_command(self, code_path, exe_path):
-        pass
+        temp_dir = tempfile.gettempdir()
+        compile_cmd = screen_str(self.compiler_path) \
+                      + " -Fo" + temp_dir \
+                      + " -FE" + str(exe_path) \
+                      + " " + code_path
+        return compile_cmd
 
     @staticmethod
     def get_extension(**kwargs):
---
+++ server_project/server/build_bot_project/unit_tests/test_fp_build_bot.py
@@ -0,0 +1,52 @@
+import os
+import unittest
+
+import pytest
+from build_bot_project.build_bot import BuildBot
+from build_bot_project.languages.fp_language import FPLanguage
+from common.cmd_utils import shell, split_lines
+
+
+test_name = __name__
+
+
+@pytest.mark.usefixtures("test_build_directory_fixture", "test_config_fixture")
+class FPBuildBotTests(unittest.TestCase):
+    def setUp(self):
+        self.test_file_name = "fp_basic"
+        self.test_src_name = self.test_file_name + ".pas"
+        self.test_exe_name = self.test_file_name + ".exe"
+
+        self.test_src_file_path = os.path.join(self.src_code_dir, self.test_src_name)
+
+        self.output_dir = os.path.join(self.build_dir, "cfg_output_dir")
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+        self.test_exe_path = self.output_dir
+
+        self.language = FPLanguage(self.config_parser)
+        self.build_bot = BuildBot()
+        self.build_bot.set_language(self.language)
+
+    def test_are_all_fixture_attributes_set(self):
+        assert hasattr(self, "build_dir")
+        assert hasattr(self, "build_dir_bin")
+        assert hasattr(self, "src_code_dir")
+        assert hasattr(self, "config_parser")
+        assert hasattr(self, "is_config_read_ok")
+
+    def test_can_successfully_run_build_bot(self):
+        (ret_code, out, err) = self.build_bot.build(self.test_src_file_path, self.test_exe_path)
+        self.assertEquals(ret_code, 0)
+
+    def test_does_exist_exe_file_created_by_build_bot(self):
+        self.build_bot.build(self.test_src_file_path, self.test_exe_path)
+        self.assertTrue(os.path.exists(self.test_exe_path))
+
+    def test_can_successfully_run_created_exe_file(self):
+        self.build_bot.build(self.test_src_file_path, self.test_exe_path)
+        test_exe = os.path.join(self.output_dir, self.test_exe_name)
+        (ret_code, out, err) = shell(test_exe)
+        self.assertEquals(split_lines(out).pop(0), "Hello, world.")
+        self.assertEquals(ret_code, 0)
+
